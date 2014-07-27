cb99faa8598a107007ee6c862084c40970172ade
andreasDL <andreasdelille@hotmail.Com>
2014-07-27 22:02:53 +0200

    screenrc added

--- config.sh
+++ config.sh
@@ -4,12 +4,13 @@ mkdir files;
 
 cp -v  /home/drew/.bashrc 				files/;
 cp -v  /home/drew/.xinitrc 				files/;
+cp -v  /home/drew/.screenrc				files/;
 cp -v  /etc/systemd/logind.conf     			files/;
 cp -v  /etc/X11/xorg.conf.d/10-evdev.conf 		files/; #keyboard settings
 cp -v  /etc/slim.conf               			files/;
 cp -v  /etc/sudoers 					files/;
 cp -v  /etc/fstab 					files/; #example has no real use
-
+i
 cp -v  /home/drew/.rtorrent.rc 				files/;
 cp -v  /usr/local/bin/ydl 				files/; #download script
 
