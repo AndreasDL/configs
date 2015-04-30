528b0c758cc4b23340196f505f7224294e3f3f52
andreasDL <andreasdelille@hotmail.Com>
2015-04-30 14:07:54 +0200

    screenrc + firefox at workspace T

--- files/rc.lua
+++ files/rc.lua
@@ -481,9 +481,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+    -- Set Firefox to always map on tags number 5 of screen 1.
+    { rule = { class = "Firefox" },   properties = { tag = tags[1][5] } }
 }
 -- }}}
 
@@ -559,7 +558,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
-
 
 -- battery warning
 -- created by bpdp
